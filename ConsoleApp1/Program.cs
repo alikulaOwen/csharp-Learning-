1664df471ffc2af128d9e4108a3fb0cd6d2149f9
Caldewod Owen Alikula <alikulaowen@gmail.com>
2024-04-14 19:14:23 +0300

    simple reverse string app

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more informatio
+//
+//
+//
+using System;
+namespace  ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.Write("Enter your message: ");
+           string str = Console.ReadLine() ?? string.Empty;
+           string revStr = ReverseString(str) ;
+           Console.WriteLine("Reversed is equal to: ==>" + revStr);
+           Console.ReadLine();
+
+        }
+
+        static string ReverseString(string str)
+        {
+            char[] charList = str.ToCharArray();
+            Array.Reverse(charList);
+            return new string(charList);
+        }
+    }
+};                                   
